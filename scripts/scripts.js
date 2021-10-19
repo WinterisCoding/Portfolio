cf684be3ff6e8c176d69983ac2ffcefb3f5a0b49
WinterisCoding <elisabethwinter.writes@gmail.com>
2021-10-19 18:10:03 -0400

    second pass styling, assets in place

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,6 +2,7 @@ const portfolioApp = {};
 
 portfolioApp.form = document.querySelector('.contactForm');
 portfolioApp.projectImage = document.querySelectorAll('.projectImage')
+portfolioApp.closeButton = document.querySelectorAll('.fa-times')
 
 portfolioApp.submitForm = () => {
   portfolioApp.form.addEventListener('submit', function(event){
@@ -13,15 +14,26 @@ portfolioApp.submitForm = () => {
 portfolioApp.showInfo = () => {
   for (let i =0; i < portfolioApp.projectImage.length; i++){
     portfolioApp.projectImage[i].addEventListener('click', function(event){
-      console.log(event.target.parentNode)
-    })
+      const project = event.target.parentNode;
+      const overlay = project.childNodes[1];
+      overlay.classList.add('active');
+    });
   }
-  ;
+}
+
+portfolioApp.closeInfo = () => {
+  for (let i = 0; i < portfolioApp.closeButton.length; i++){
+    portfolioApp.closeButton[i].addEventListener('click', function(event){
+      const project = event.target.parentNode;
+      project.classList.remove('active');
+    });
+  }
 }
 
 portfolioApp.init = () => {
   portfolioApp.submitForm();
   portfolioApp.showInfo();
+  portfolioApp.closeInfo();
 }
 
 portfolioApp.init()
